5e1963fa564cffcef0d4b38a416722cbea51ee4f
duqunqiong <dqq_1029@163.com>
2020-06-16 17:32:56 +0800

    项目资料内容

---
+++ exepriesIdea/DMP/spark_wareHouse/TypeUtils.scala
@@ -0,0 +1,21 @@
+/**
+ * 类型转换工具类
+ */
+object TypeUtils {
+  // 转换Int类型
+  def str2Int(str:String):Int={
+    try {
+      str.toInt
+    }catch {
+      case _:Exception=> 0
+    }
+  }
+  // 转换Int类型
+  def str2Double(str:String):Double={
+    try {
+      str.toDouble
+    }catch {
+      case _:Exception=> 0.0
+    }
+  }
+}
